a7e5acbe2a986290f3e2f154e2a4902b40fa7c1f
PlumDong <1695165447@qq.com>
2020-12-07 20:35:01 +0800

    项目初始创建结构

---
+++ gulimall-member/src/test/java/com/sdtbu/gulimallmember/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdtbu.gulimallmember.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
