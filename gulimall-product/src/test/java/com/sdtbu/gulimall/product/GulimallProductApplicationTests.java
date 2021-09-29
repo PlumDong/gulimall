70deec25e856c73c73c2a7fc840addb838de49a5
PlumDong <1695165447@qq.com>
2021-09-29 12:52:15 +0800

    添加OSS 云对象存储

--- gulimall-product/src/test/java/com/sdtbu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/sdtbu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,12 +1,21 @@
 package com.sdtbu.gulimall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
 import com.sdtbu.gulimall.product.entity.BrandEntity;
 import com.sdtbu.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Repository;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,10 +27,32 @@ class GulimallProductApplicationTests {
 	@Test
 	void contextLoads() {
 		BrandEntity brandEntity = new BrandEntity();
-
 		brandEntity.setName("中");
 		brandService.save(brandEntity);
 		System.out.println("保存成功！！！");
+	}
+
+	@Resource
+	OSSClient ossClient;
+
+	@Test
+	public void testUpaload() throws FileNotFoundException {
+//		// yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+//		String endpoint = "oss-cn-shanghai.aliyuncs.com";
+//		// 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+//		String accessKeyId = "LTAI5t5kdQ88dggD5Jj4C9BP";
+//		String accessKeySecret = "UUyDLxjoJzrTKvq0WXHEd1NbXElQdx";
+
+		// 创建OSSClient实例。
+		//OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+		// 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+		InputStream inputStream = new FileInputStream("C:\\Users\\Administrator\\Pictures\\Control4k.jpg");
+		// 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+		ossClient.putObject("goosemall", "exampledir/Control4k.jpg", inputStream);
+
+		// 关闭OSSClient。
+		ossClient.shutdown();
 
 	}
 
